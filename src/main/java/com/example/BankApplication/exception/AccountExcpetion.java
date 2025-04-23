063d2a50416068dc1dea5674ea3a85a3fdf2b385
RenaldoNika <roniii2017.com>
2025-04-23 22:10:17 +0200

    first commit

---
+++ src/main/java/com/example/BankApplication/exception/AccountExcpetion.java
@@ -0,0 +1,16 @@
+package com.example.BankApplication.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AccountExcpetion {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+}
